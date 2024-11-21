e69d26acd3c921f2fe146f31ce809567a9de5a11
gustarlq <161773785+gustarlq@users.noreply.github.com>
2024-11-21 19:50:21 -0300

    Add files via upload

--- commom.js
+++ commom.js
@@ -1,12 +1,29 @@
-const getCSS = (variavel) => {
-    const bodyStyles = getComputedStyle(document.body)
-    return bodyStyles.getPropertyValue(variavel)
-}
-
-const tickConfig = {
-    family: getCSS('--font'),
-    size: 16,
-    color: getCSS('--primary-color')
-}
-
-export {getCSS, tickConfig}
+async function objetivosUsuarios() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/razoes-globais.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const objetivosUsuarios = Object.keys(dados)
+    const quantidadeUsuarios = Object.values(dados)
+  
+  const data = [
+    {
+      labels: objetivosUso,
+      values: quantidadeUsuarios,
+      type: 'pie'
+    }
+  ]
+  
+  const layout = 
+    {
+      height: 600,
+      width: 870,
+    }
+  
+  const grafico = document.createElement('div')
+  grafico.className = 'grafico'
+  document.getElementById('graficos-container').appendChild(grafico)
+  Plotly.newPlot(grafico, data, layout)
+  
+  }
+  
+  objetivosUsuarios()
